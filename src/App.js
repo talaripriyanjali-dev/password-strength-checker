57456b519b81850e80fd78481b528194cc5fcb79
User Name <user@example.com>
2026-02-24 07:15:32 +0530

    feat: add complete password strength checker React project with UI and validation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PasswordStrength from "./PasswordStrength";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <PasswordStrength />
+    </div>
+  );
+}
+
+export default App;
